616af8c74a4e9e1d7d28899cd5c5c514bbf9dbb7
rajon-git <rajon.zhsust15@gmail.com>
2024-01-15 14:22:42 +0600

    initialstate create for filter and also reducer folder add

---
+++ src/redux/filters/reducer.js
@@ -0,0 +1,12 @@
+import initialState from "./initialState";
+
+const reducer = (state =initialState, action) =>{
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}

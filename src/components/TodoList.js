2754e8b632bc66a2c9daae43f852f800b78967c3
rajon-git <rajon.zhsust15@gmail.com>
2024-01-21 15:54:12 +0600

    status changed add in todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,25 @@ import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 export default function TodoList() {
+    const filters = useSelector((state)=>state.filters);
     const todos = useSelector((state)=> state.todos);
     
     return (
         <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-            {todos.map((todo)=> (<Todo todo={todo} key={todo.id}/>))}
+            {todos
+            .filter(todo=>{
+                const {status} = filters;
+                switch (status) {
+                   
+                    
+                case 'Incompleted':
+                    return !todo.completed;
+                
+                    default:
+                        return true;
+                }
+            })
+            .map((todo)=> (<Todo todo={todo} key={todo.id}/>))}
         </div>
     );
 }

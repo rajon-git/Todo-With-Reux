ec8ac6a4f9c2e3bcbf9d4e2867182dd692efe515
rajon-git <rajon.zhsust15@gmail.com>
2024-01-22 16:26:48 +0600

    task add functionality add and also dispatch this

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import tickImage from "../assets/images/double-tick.png";
 import noteImage from "../assets/images/notes.png";
 import plusImage from "../assets/images/plus.png";
+import { useDispatch } from "react-redux";
+import { added } from "../redux/todos/actions";
 
 export default function Header() {
+    const dispatch= useDispatch();
+    const [input,setInput] = useState('');
+
+    const handleInput = (e)=>{
+        setInput(e.target.value);
+    }
+
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        dispatch(added(input));
+        setInput(" ");
+    }
     return (
         <div>
-            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md" onSubmit={submitHandler}>
                 <img src={noteImage} className="w-6 h-6" alt="Add todo" />
                 <input
                     type="text"
                     placeholder="Type your todo"
                     className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+                    value={input}
+                    onChange={handleInput}
                 />
                 <button
                     type="submit"
